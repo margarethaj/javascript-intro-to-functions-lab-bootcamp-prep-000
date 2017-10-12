4d4142ef8293a9165e55d8520a0ca315d6988bb9
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-12 23:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
   }
   else if (string.toUpperCase() === string){
-    return "I can\'t hear you"
+    return "YES INDEED!"
   }
   else {
     return "I love you, too."
